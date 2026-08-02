9336a98d1e1801de87e25b4b8a2e561dbd8b1eca
FlipStart <dylancarey6@icloud.com>
2026-08-01 21:22:42 -0500

    Version 2.0

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ const scheme   = "flipstart";
 const config: ExpoConfig = {
   name:                "FlipStart",
   slug:                "flipstart",
-  version:             "1.2.3",
+  version:             "2.0",
   orientation:         "portrait",
   icon:                "./assets/images/icon.png",
   scheme,
